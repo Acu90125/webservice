df95749faa28392d90e0c069f1901e1a0441d8c6
Acu90125 <mgomez1408@gmail.com>
2015-06-26 12:55:57 -0300

    primera subida

---
+++ llamando/consulta.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.OleDb;
+using System.Xml;
+
+namespace llamando
+{
+    public partial class consulta : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BindUserDetails();
+        }
+
+        private void BindUserDetails()
+        {
+            string UName = txtUserName.Text; //Textbox to read the values
+            localhost.Service objService = new localhost.Service();
+            //webservice aliase name with object
+            DataSet ds = new DataSet();
+            XmlElement elem = objService.GetUserDetails(UName);
+            if (elem != null)
+            {
+                XmlNodeReader nodereader = new XmlNodeReader(elem);
+                ds.ReadXml(nodereader, XmlReadMode.Auto);
+                gvUserDetails.DataSource = ds;
+                gvUserDetails.DataBind();
+            }
+            else
+            {
+                gvUserDetails.DataSource = null;
+                gvUserDetails.DataBind();
+            }
+        }
+    }
+}
---
+++ web-service/App_Code/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public XmlElement GetUserDetails(string userName)
+    {
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ToString());
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from padron  where Apellido like @userName+'%'", con);
+        cmd.Parameters.AddWithValue("@userName", userName);
+        cmd.ExecuteNonQuery();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        // Create an instance of DataSet.
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        con.Close();
+        // Return the DataSet as an XmlElement.
+        XmlDataDocument xmldata = new XmlDataDocument(ds);
+        XmlElement xmlElement = xmldata.DocumentElement;
+        return xmlElement;
+    }
+    
+}
